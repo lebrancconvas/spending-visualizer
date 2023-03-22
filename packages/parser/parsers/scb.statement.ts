885a6a7e4df72cf1aa36d6de667d2a0e4927a6bc
lebrancconvas <lebrancconvas@gmail.com>
2023-03-22 20:16:33 +0700

    ✨ feat: add argument on main function

--- packages/parser/parsers/scb.statement.ts
+++ packages/parser/parsers/scb.statement.ts
@@ -2,6 +2,6 @@ import { StatementParser } from "../@types/StatementParser";
 import { Transaction } from "../@types/Transaction";
 
 
-export function parseSCBStatement(): Transaction[] {
+export function parseSCBStatement(content: string[][]): Transaction[] {
   return null; 
 }
